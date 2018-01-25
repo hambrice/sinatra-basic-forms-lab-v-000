240dc8a9efeeba9e135a5cfe91725f3bd7f403c0
Brad Hambrice <hambrice@uab.edu>
2018-01-25 04:31:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,4 +8,11 @@ class App < Sinatra::Base
   get '/create_puppy' do
     erb :create_puppy
   end
+
+  post '/display_puppy' do
+    dog = Dog.new(params[:name],params[:breed], params[:age])
+    @breed = dog.breed
+    @name = dog.name
+    @age = dog.age
+    erb :display_puppy
 end
